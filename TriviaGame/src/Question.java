593b724244a421b5b57c75649e7469c6570236f7
Atif Mahmud <atifmahmud95@gmail.com>
2017-06-02 01:25:11 +0600

    Reads from file

--- TriviaGame/src/Question.java
+++ TriviaGame/src/Question.java
@@ -1,6 +1,6 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
- 
 
 public class Question {
     public String question; 
@@ -8,51 +8,71 @@ public class Question {
 	public String OptionB;
 	public String OptionC;
 	public String OptionD;
-	public static String correctOption;
+	public String correctOption;
 	public static int score = 0;  // Keeps track of score 
 	
-	
-	public static void main (String [] args){
-		
-	// An array of 150 Questions
-	Question [] questionArray = new Question [150];
-	
-	// Initializing the objects
-	for(int i=0; i < 150; ++i){
-		questionArray[i] = new Question();
+	// Constructor for a new question
+	public Question (String question, String OptionA, String OptionB, String OptionC, String OptionD, String correctOption){
+		this.question = question;
+		this.OptionA = OptionA;
+		this.OptionB = OptionB;
+		this.OptionC = OptionC;
+		this.OptionD = OptionD;
+		this.correctOption = correctOption;
 	}
 	
-}
 	
+	public static void main (String [] args) throws Exception{
 	
-	
-	public static void askQuestion (Question Q){
 		
-		System.out.println(Q.question);
-		System.out.println("A)" + Q.OptionA); System.out.println("B) " + Q.OptionB);
-		System.out.println("C) " + Q.OptionC); System.out.println("D) " + Q.OptionD);
-	
-		Scanner reader = new Scanner(System.in);
-		String userInput = (reader.next());
+			Question [] questionArray = new Question [150]; // An array of 150 Questions
+			FileReader questionFile = new FileReader ("D:/Serious/Academic/Programming and Software/Java/TriviaGame/TriviaGame/src/QuestionBank.txt");	
+			BufferedReader reader = new BufferedReader(questionFile);
 		
+			
+		for (int j=0; j < 5; ++j){ // Read the fields of the question
+			
+				String q = reader.readLine();
+				String A = reader.readLine();
+				String B = reader.readLine();
+				String C = reader.readLine();
+				String D = reader.readLine();
+				String correct = reader.readLine();
 		
-		if (userInput.equals(Q.correctOption)){
-			System.out.println("Correct!");
-		    score++;
-		 }
-		
-		else{
-			System.out.printf("Incorrect! Correct option was %s\n", Q,correctOption);
+				questionArray[j] = new Question (q, A, B, C, D, correct); // Construct the new question
 		}
 		
-		System.out.print("Score = ");
-		System.out.print(score);
-		System.out.print("\n\n\n");
 		
+		
+		for (int i=0; i < 5; ++i){
+			askQuestion(questionArray[i]);
+		}
+}
+	
+public static void askQuestion(Question question){
+	System.out.println(question.question);
+	System.out.println(question.OptionA);
+	System.out.println(question.OptionB);
+	System.out.println(question.OptionC);
+	System.out.println(question.OptionD);
+	
+	Scanner scan = new Scanner(System.in);
+	String userInput = scan.next();
+	
+	if(userInput.equals(question.correctOption)){
+		System.out.println("Correct!\n");
+		score++;
 	}
 	
+	else{
+		System.out.println("Wrong\n");
+	}
+	
+	System.out.printf("Score = %d\n\n", score );
+}	
+}
 	
 	
 
 	
-}
+

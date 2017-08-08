e240db30cca103735b02fc36703a1f8b80a9dac0
AtifMahmud <atifmahmud95@gmail.com>
2017-08-08 18:44:40 +0600

    Redundant, replaced with QuestionPanel

--- TriviaGame/src/Question.java
+++ TriviaGame/src/Question.java
@@ -1,17 +1,10 @@
 import java.util.*;
 import java.io.*;
-import javax.swing.JOptionPane;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
-import javax.swing.SwingUtilities;
-import java.lang.Integer;
-
+import javax.swing.JButton;
+import javax.swing.*;
+import java.awt.CardLayout;
 
 public class Question{
     public static String question; 
@@ -20,6 +13,7 @@ public class Question{
 	public static String OptionC;
 	public static String OptionD;
 	public static String correctOption;
+	JPanel QuestionPanel;
 	public GUI MyGUI;
 	
 	
@@ -31,6 +25,23 @@ public class Question{
 		this.OptionC = OptionC;
 		this.OptionD = OptionD;
 		this.correctOption = correctOption;
+	
+		JPanel QuestionPanel = new JPanel();
+		QuestionPanel.setLayout(new CardLayout());
+		JTextField QuestionText = new JTextField(question);
+		JButton A = new JButton(OptionA);
+		JButton B = new JButton(OptionB);
+		JButton C = new JButton(OptionC);
+		JButton D = new JButton(OptionD);
+		JButton correct = new JButton(correctOption);
+		
+		QuestionPanel.add(QuestionText);
+		QuestionPanel.add(A);
+		QuestionPanel.add(B);
+		QuestionPanel.add(C);
+		QuestionPanel.add(D);
+		QuestionPanel.add(correct);
+	
 	}
 }
 	

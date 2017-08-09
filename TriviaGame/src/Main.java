eae46dce89e3d96b0d5c74ce380371397f3cb43e
AtifMahmud <atifmahmud95@gmail.com>
2017-08-09 15:31:17 +0600

    GridLayout done

--- TriviaGame/src/Main.java
+++ TriviaGame/src/Main.java
@@ -23,7 +23,7 @@ import java.awt.Dimension;
 import java.util.Random;
 import java.io.*;
 import javax.swing.SwingUtilities;
-import javax.swing.BoxLayout;
+import java.awt.GridLayout;
 import java.awt.CardLayout;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
@@ -33,188 +33,174 @@ import javax.swing.SwingConstants;
 import javax.sound.sampled.*;
 import java.awt.Toolkit;
 import javax.swing.UIManager;
+import java.awt.Component;
 
 public class Main extends JFrame {
 	
 	public static int score = 0;
 	
 	public static void main (String [] args)  throws Exception {
-		 int size = 9;       // Number of questions in the file
-		 int numQuestions;
+		 int size = 10;       // Number of questions in the file
 		 
 		 
-		 try {
-			// Prompt the user for number of questions; use JOptionPane
-			do{
-				numQuestions = Integer.parseInt(JOptionPane.showInputDialog("Welcome to Atif's Game. Please enter number of questions you want to be asked."));
-			}
+		 	ArrayList <JPanel> questionList = new ArrayList<JPanel>();
+		
+		
+			Dimension FrameSize = new Dimension(500, 500);
+			Dimension ButtonSize = new Dimension(20,10);
 			
-			while
-				(numQuestions < 1 || numQuestions > 5);
-		 }
-		 
-		 catch(NumberFormatException n){
-			 return;
-		 }
-		
-		
-		ArrayList <JPanel> questionList = new ArrayList<JPanel>();
-		
-		
-		Dimension FrameSize = new Dimension(500, 500);
-		Dimension ButtonSize = new Dimension(100,200);
-		
-		JFrame Frame = new JFrame();
-		JPanel ContentPanel = new JPanel();
-		
-		CardLayout card = new CardLayout();
-		ContentPanel.setLayout(card);
-		
-		JButton A = null,B=null,C=null,D = null;
-		
-		// One case where we need the try-catch block is if the file doesn't exist
-		try {
-			FileReader questionFile = new FileReader ("D:/Serious/Academic/Programming and Software/Java/TriviaGame/TriviaGame/src/QuestionBank.txt");	
-			BufferedReader reader = new BufferedReader(questionFile);
-		
-			// Read the fields of the question, and construct a new one	
-			for (int j=0; j < size; j++){ 
-				 	int k = j;	
-					JTextField q = new JTextField(reader.readLine());
-					A = new JButton(reader.readLine());
-					B = new JButton(reader.readLine());
-					C = new JButton(reader.readLine());
-				    D = new JButton(reader.readLine());
-				    String correct = reader.readLine();
-				    QuestionPanel AskPanel = new QuestionPanel (q, A, B, C, D, correct);
-				    
-				    AskPanel.add(q);
-				    AskPanel.add(A);
-				    AskPanel.add(B);
-				    AskPanel.add(C);
-				    AskPanel.add(D);
-				    
-				    A.setSize(ButtonSize);
-				    B.setSize(ButtonSize);
-				    C.setSize(ButtonSize);
-				    D.setSize(ButtonSize);
-				    
-				    q.setEditable(false);
-					questionList.add(AskPanel);
-					ContentPanel.add(AskPanel, Integer.toString(j));
-					
-					A.addActionListener(new ActionListener(){
-						@Override
-						public void actionPerformed(ActionEvent a) {
-							if(AskPanel.correctOption.equals("A")){
-								score++;
-								Toolkit.getDefaultToolkit().beep();
-								UIManager.put("OptionPane.background", Color.GREEN);
-								UIManager.put("OptionPane.foreground", Color.BLUE);
-								JOptionPane.showMessageDialog(null, "Correct!, Score = " + Integer.toString(score));
-								
-							}
-							
-							else {
-								UIManager.put("OptionPane.background", Color.RED);
-								JOptionPane.showMessageDialog(null, "Wrong!, Score = " + Integer.toString(score));
-							}
-							
-							card.show(ContentPanel, Integer.toString(k+1));
-
+			JFrame Frame = new JFrame();
+			JPanel ContentPanel = new JPanel();
+			
+			CardLayout card = new CardLayout();
+			ContentPanel.setLayout(card);
+			
+			JButton A = null,B=null,C=null,D = null;
+			
+			// One case where we need the try-catch block is if the file doesn't exist
+			try {
+				FileReader questionFile = new FileReader ("D:/Serious/Academic/Programming and Software/Java/TriviaGame/TriviaGame/src/QuestionBank.txt");	
+				BufferedReader reader = new BufferedReader(questionFile);
+			
+				// Read the fields of the question, and construct a new one	
+				for (int j=0; j < size; j++){ 
+					 	int k = j;	
+						JTextField q = new JTextField(reader.readLine());
+						A = new JButton(reader.readLine());
+						B = new JButton(reader.readLine());
+						C = new JButton(reader.readLine());
+					    D = new JButton(reader.readLine());
+					    String correct = reader.readLine();
+					    QuestionPanel AskPanel = new QuestionPanel (q, A, B, C, D, correct);
+					    
+					    
+					    A.setSize(ButtonSize);
+					    B.setSize(ButtonSize);
+					    C.setSize(ButtonSize);
+					    D.setSize(ButtonSize);
+					    
+					    AskPanel.setLayout(new GridLayout(2,2,10,10)); // 2 rows and 2 columns, hgap and vgap of 10
+					    
+					    AskPanel.add(q);
+					    AskPanel.add(A);
+					    AskPanel.add(B);
+					    AskPanel.add(C);
+					    AskPanel.add(D);
+					    
 				
-						}
-					});	
-
-					B.addActionListener(new ActionListener(){
-						@Override
-						public void actionPerformed(ActionEvent a) {
-							if(AskPanel.correctOption.equals("B")){
-								score++;
-								Toolkit.getDefaultToolkit().beep();
-								UIManager.put("OptionPane.background", Color.GREEN);
-								JOptionPane.showMessageDialog(null, "Wrong!, Score = " + Integer.toString(score));
+					    
+					    q.setEditable(false);
+						questionList.add(AskPanel);
+						ContentPanel.add(AskPanel, Integer.toString(j));
+						
+						A.addActionListener(new ActionListener(){
+							@Override
+							public void actionPerformed(ActionEvent a) {
+								if(AskPanel.correctOption.equals("A")){
+									score++;
+									Toolkit.getDefaultToolkit().beep();
+									UIManager.put("OptionPane.background", Color.GREEN);
+									UIManager.put("OptionPane.foreground", Color.BLUE);
+									JOptionPane.showMessageDialog(null, "Correct!, Score = " + Integer.toString(score));
+									
+								}
 								
-							}
-							
-							else {
-								UIManager.put("OptionPane.background", Color.RED);
-								JOptionPane.showMessageDialog(null, "Wrong!, Score = " + Integer.toString(score));
+								else {
+									UIManager.put("OptionPane.background", Color.RED);
+									JOptionPane.showMessageDialog(null, "Wrong!, Score = " + Integer.toString(score));
+								}
 								
-							}
-							
-							card.show(ContentPanel, Integer.toString(k+1));
-						}
-					});	
-					
-					C.addActionListener(new ActionListener(){
-						@Override
-						public void actionPerformed(ActionEvent a) {
-							if(AskPanel.correctOption.equals("C")){
-								score++;
-								Toolkit.getDefaultToolkit().beep();
-								UIManager.put("OptionPane.background", Color.GREEN);
-								JOptionPane.showMessageDialog(null, "Correct!, Score = " + Integer.toString(score));
-								
-							  
-							}
-							
-							else {
-								UIManager.put("OptionPane.background", Color.RED);
-								JOptionPane.showMessageDialog(null, "Wrong!, Score = " + Integer.toString(score));
-								
-							}
-							
-							card.show(ContentPanel, Integer.toString(k+1));
-						}
-					});	
-					
-					D.addActionListener(new ActionListener(){
-						@Override
-						public void actionPerformed(ActionEvent a) {
-							if(AskPanel.correctOption.equals("D")){
-								score++;
-								Toolkit.getDefaultToolkit().beep();
-								UIManager.put("OptionPane.background", Color.GREEN);
-								JOptionPane.showMessageDialog(null, "Correct!, Score = " + Integer.toString(score));
-								
-							}
-							
-							else {
-								UIManager.put("OptionPane.background", Color.RED);
-								JOptionPane.showMessageDialog(null, "Wrong!, Score = " + Integer.toString(score));
-								
-							}
-							
-							card.show(ContentPanel, Integer.toString(k+1));
-						}
-					});
-
-			}
-		}
-	  
-
-	    catch (IOException e){
-			System.out.println("We've hit a problem. Please try again\n\n");
-			System.exit(0);            // Add this to prevent program from running further when we hit the exception
-	  } 
-		
+								card.show(ContentPanel, Integer.toString(k+1));
 	
-		Frame.add(ContentPanel);
-		Frame.setSize(FrameSize);
-		Frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		Frame.setVisible(true);
+					
+							}
+						});	
 	
-		
-		SwingUtilities.invokeLater(new Runnable(){
-				@Override
-				public void run(){
-					new Main();
+						B.addActionListener(new ActionListener(){
+							@Override
+							public void actionPerformed(ActionEvent a) {
+								if(AskPanel.correctOption.equals("B")){
+									score++;
+									Toolkit.getDefaultToolkit().beep();
+									UIManager.put("OptionPane.background", Color.GREEN);
+									JOptionPane.showMessageDialog(null, "Wrong!, Score = " + Integer.toString(score));
+									
+								}
+								
+								else {
+									UIManager.put("OptionPane.background", Color.RED);
+									JOptionPane.showMessageDialog(null, "Wrong!, Score = " + Integer.toString(score));
+									
+								}
+								
+								card.show(ContentPanel, Integer.toString(k+1));
+							}
+						});	
+						
+						C.addActionListener(new ActionListener(){
+							@Override
+							public void actionPerformed(ActionEvent a) {
+								if(AskPanel.correctOption.equals("C")){
+									score++;
+									Toolkit.getDefaultToolkit().beep();
+									UIManager.put("OptionPane.background", Color.GREEN);
+									JOptionPane.showMessageDialog(null, "Correct!, Score = " + Integer.toString(score));
+									
+								  
+								}
+								
+								else {
+									UIManager.put("OptionPane.background", Color.RED);
+									JOptionPane.showMessageDialog(null, "Wrong!, Score = " + Integer.toString(score));
+									
+								}
+								
+								card.show(ContentPanel, Integer.toString(k+1));
+							}
+						});	
+						
+						D.addActionListener(new ActionListener(){
+							@Override
+							public void actionPerformed(ActionEvent a) {
+								if(AskPanel.correctOption.equals("D")){
+									score++;
+									Toolkit.getDefaultToolkit().beep();
+									UIManager.put("OptionPane.background", Color.GREEN);
+									JOptionPane.showMessageDialog(null, "Correct!, Score = " + Integer.toString(score));
+									
+								}
+								
+								else {
+									UIManager.put("OptionPane.background", Color.RED);
+									JOptionPane.showMessageDialog(null, "Wrong!, Score = " + Integer.toString(score));
+									
+								}
+								
+								card.show(ContentPanel, Integer.toString(k+1));
+							}
+						});
+	
 				}
-			});
-
-	}
+			}
+		  
+		    catch (IOException e){
+				System.out.println("We've hit a problem. Please try again\n\n");
+				System.exit(0);            // Add this to prevent program from running further when we hit the exception
+		  } 
+		
+			Frame.add(ContentPanel);
+			Frame.setSize(FrameSize);
+			ContentPanel.setSize(FrameSize);
+			Frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+			Frame.setVisible(true);
+	
+			SwingUtilities.invokeLater(new Runnable(){
+					@Override
+					public void run(){
+						new Main();
+					}
+				});
+	
+		}
 	
 }
-	
-
